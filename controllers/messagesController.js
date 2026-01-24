3c2d603cc45321799d5ede8a2d4e20fda5224f4a
loops <128599880+loopsc@users.noreply.github.com>
2026-01-25 01:38:10 +1300

    (feat) Implement validation rules.
    Form saves drafts when encountered error.

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,11 +1,23 @@
-const db = require("../db/queries")
+const db = require("../db/queries");
+const { body, validationResult, matchedData } = require("express-validator");
+
+const validationRules = [
+    body("messageName")
+        .trim()
+        .isLength({ min: 1, max: 20 })
+        .withMessage("Name must be between 1-20 characters"),
+    body("messageText")
+        .trim()
+        .isLength({ min: 1, max: 200 })
+        .withMessage("Text must be between 1-200 characters"),
+];
 
 async function getAllMessages(req, res) {
     const messages = await db.getAllMessages();
     res.render("index", {
         title: "Message Board",
-        messages: messages
-    })
+        messages: messages,
+    });
 }
 
 async function getMessageById(req, res) {
@@ -14,7 +26,7 @@ async function getMessageById(req, res) {
     if (!message) return res.status(404).send("Message not found");
 
     res.render("message", {
-        message
+        message,
     });
 }
 
@@ -22,13 +34,29 @@ async function getCreateMessage(req, res) {
     res.render("form");
 }
 
-async function postCreateMessage(req, res) {
-    let messageName = req.body.messageName;
-    let messageText = req.body.messageText;
+const postCreateMessage = [
+    validationRules,
+    async (req, res, next) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).render("form", {
+                    errors: errors.array(),
+                    oldInput: req.body,
+                });
+            }
+            const { messageName, messageText } = matchedData(req);
+            await db.insertMessage(messageName, messageText);
+            res.redirect("/");
+        } catch (err) {
+            next(err)
+        }
+    },
+];
 
-    await db.insertMessage(messageName, messageText)
-
-    res.redirect("/")
-}
-
-module.exports = { getMessageById, getAllMessages, getCreateMessage, postCreateMessage };
+module.exports = {
+    getMessageById,
+    getAllMessages,
+    getCreateMessage,
+    postCreateMessage,
+};
